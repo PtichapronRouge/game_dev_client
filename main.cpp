c9f6e656919b540cf4a43a07a7db8e4e25d2230e
Ptichapron <benjamin.mouscadet@supelec.fr>
2017-08-25 12:40:58 +0200

    Correction sur la branche master

--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
-#include <iostream>
-#include "headers/application.hpp"
-#include "headers/displaymanager.hpp"
-
-
-int main()
+int main(int argc, char* argv[])
 {
-    DisplayManager dm(std::cout);
-    dm << "test";
-    dm << "test";
-
     return 0;
 }
